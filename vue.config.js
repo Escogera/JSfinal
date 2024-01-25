050601423313651319973ae83d06b6a41e7f787d
Vladimir <maostyle@mail.ru>
2024-01-25 15:00:30 +0300

    try to deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/interno-vuecli/' : '/'
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/JSfinal/" : "/",
+};
